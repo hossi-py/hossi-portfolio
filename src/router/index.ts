a6152b16aa0533d10db7079cc804b09a95204209
hossi-py <68405946+hossi-py@users.noreply.github.com>
2025-03-25 11:05:48 +0900

    Custom component

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import customRoutes from './customRoutes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    ...(customRoutes as RouteRecordRaw[]),
+    ...(customRoutes as RouteRecordRaw[]), // Custom 컴포넌트용 라우트
     {
       path: '/',
       name: 'main',
